618a49cfc9d7a99dd9aefead8e8c4502d4814bbc
Pranav Byakod <pranav.byakod@gmail.com>
2021-07-20 21:17:28 -0700

    addEngineer and addIntern functions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = reequire('fs');
 const inquirer = require('inquirer');
+const Engineer = require('./lib/engineer_class');
+const Intern = require('./lib/intern_class');
 const Manager = require('./lib/manager_class');
 
 const employees = [];
@@ -48,6 +50,42 @@ var engineer_questions = [
         message: 'Enter your GITHUB username',
         name: 'github_id',
     },
+    {
+        type: 'list',
+        message: 'Do you want to ADD another engineer?',
+        choices: ['Yes', 'No'],
+        name: 'another'
+    }
+]
+
+var intern_questions = [
+    {
+        type: 'input',
+        message: 'Enter your NAME',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Enter your EMPLOYEE ID',
+        name: 'employeee_id',
+    },
+    {
+        type: 'input',
+        message: 'Enter a valid EMAIL ADDRESS',
+        name: 'email_address',
+    },
+    {
+        type: 'input',
+        message: 'Enter the SCHOOL NAME of the intern',
+        name: 'school_name',
+    },
+    {
+        type: 'list',
+        message: 'Do you want to ADD another intern?',
+        choices: ['Yes', 'No'],
+        name: 'another'
+    }
+
 ]
 
 function initialize() {
@@ -66,7 +104,7 @@ function initialize() {
 function updateEmployees() {
     inquirer.prompt([
         {
-            type: 'input',
+            type: 'list',
             message: 'Are you adding an ENGINEER or an INTERN?',
             choices: ['Engineer', 'Intern'],
             name: 'position'
@@ -79,8 +117,36 @@ function updateEmployees() {
                 addEngineer();
             case 'Intern':
                 addIntern();
-            default:
-                break;
         }
     })
+}
+
+function addEngineer() {
+    inquirer.prompt(engineer_questions)
+        .then(answers => {
+            var {name, employee_id, email_address, office_number} = answers;
+            var person = new Engineer(name, employee_id, email_address, office_number);
+            employees.push(person);
+        })
+        switch(another) {
+            case 'Yes':
+                addEngineer();
+            case 'No':
+                generateHTML();
+        }
+}
+
+function addIntern() {
+    inquirer.prompt(intern_questions)
+        .then(answers => {
+            var {name, employee_id, email_address, office_number} = answers;
+            var person = new Intern(name, employee_id, email_address, office_number);
+            employees.push(person);
+        })
+        switch(another) {
+            case 'Yes':
+                addIntern();
+            case 'No':
+                generateHTML();
+        }
 }
