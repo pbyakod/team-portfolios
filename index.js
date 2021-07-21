dcca46af345a2f6a418ac95ba7e43d30b65a53e6
Pranav Byakod <pranav.byakod@gmail.com>
2021-07-20 17:51:29 -0700

    initialize and updateEmployee functions

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const inquirer = require('inquirer');
 const fs = reequire('fs');
+const inquirer = require('inquirer');
+const Manager = require('./lib/manager_class');
+
+const employees = [];
 
 var prompt_questions = [
     {
@@ -23,3 +26,30 @@ var prompt_questions = [
         name: 'office_number',
     },
 ]
+
+function initialize() {
+    inquirer.prompt(prompt_questions)
+        .then(answers => {
+            var {team_manager, employee_id, email_address, office_number} = answers;
+            var person = new Manager(team_manager, employee_id, email_address, office_number);
+            employees.push(person);
+            updateEmployees();
+        })
+        .catch(err => {
+            console.log('ERROR!', err);
+        });
+}
+
+function updateEmployees() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Are you adding an ENGINEER or an INTERN?',
+            choices: ['Engineer', 'Intern', 'None'],
+            name: 'position'
+        }
+    ])
+    .then(answer => {
+        var {position} = answer;
+    })
+}
